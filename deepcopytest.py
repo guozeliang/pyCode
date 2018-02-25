67d9be62a0724b68830d7fe98d784df7aab499f8
guozeliang <932356614@qq.com>
2018-02-25 17:57:26 +0800

    深拷贝与浅拷贝
    python深拷贝与浅拷贝

---
+++ deepcopytest.py
@@ -0,0 +1,21 @@
+import copy
+
+a = [1,2,3,4,['a','b']]
+
+b = a
+
+c = copy.copy(a)
+d = copy.deepcopy(a)
+
+print(id(a))
+print(id(b))
+print(id(c))
+
+a.append(5)
+a[4].append('c')
+
+print('a= ', a)
+print('b= ', b)
+print('c= ', c)
+print('d= ', d)
+
