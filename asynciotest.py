cce77c1b0e5c86c4bd509d01c96ea426ca75d265
guozeliang <932356614@qq.com>
2018-02-27 09:02:39 +0800

    异步IO -asyncio

---
+++ asynciotest.py
@@ -0,0 +1,50 @@
+#异步IO例子：适配Python3.4，使用asyncio库
+import asyncio
+import threading
+'''
+@asyncio.coroutine
+def hello(index):
+    print('hello world! index=%s,thread=%s' %(index,threading.current_thread()))
+    yield from asyncio.sleep(1)
+    print('hello again! index=%s,thread=%s' %(index,threading.current_thread()))
+
+loop = asyncio.get_event_loop()
+tasks = [hello(2),hello(1)]
+loop.run_until_complete(asyncio.wait(tasks))
+loop.close()
+'''
+
+
+# 异步IO例子：适配Python3.5，使用async和await关键字
+async def hello(index):       # 通过关键字async定义协程
+    print('Hello world! index=%s, thread=%s' % (index, threading.currentThread()))
+    await asyncio.sleep(1)     # 模拟IO任务
+    print('Hello again! index=%s, thread=%s' % (index, threading.currentThread()))
+
+loop = asyncio.get_event_loop()     # 得到一个事件循环模型
+tasks = [hello(1), hello(2)]        # 初始化任务列表
+loop.run_until_complete(asyncio.wait(tasks))    # 执行任务
+loop.close()                        # 关闭事件循环列表
+
+# http://python.jobbole.com/86481/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
