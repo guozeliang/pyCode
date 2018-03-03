33fe4b2d1ee0d4a0dbd83689babc61fd0777c3c2
guozeliang <932356614@qq.com>
2018-03-03 10:36:19 +0800

    并发实现的异步编程

---
+++ asyncTest.py
@@ -0,0 +1,82 @@
+'''
+这个例子里包含的几个协程，代表着火箭发射的倒计时，并且看起来是同时开始的。
+这是通过并发实现的异步编程；3个不同的协程将分别独立运行，并且都在同一个线程内完成。
+'''
+
+import datetime
+import heapq
+import types
+import time
+
+
+class Task:
+
+    def __init__(self, wait_until, coro):
+        self.coro = coro
+        self.waiting_until = wait_until
+
+    def __eq__(self, other):
+        return self.waiting_until == other.waiting_until
+
+    def __lt__(self, other):
+        return self.waiting_until < other.waiting_until
+
+
+class SleepingLoop:
+
+    def __init__(self, *coros):
+        self._new = coros
+        self._waiting = []
+
+    def run_until_complete(self):
+        # Start all the coroutines.
+        for coro in self._new:
+            wait_for = coro.send(None)
+            heapq.heappush(self._waiting, Task(wait_for, coro))
+        # Keep running until there is no more work to do.
+        while self._waiting:
+            now = datetime.datetime.now()
+            # Get the coroutine with the soonest resumption time.
+            task = heapq.heappop(self._waiting)
+            if now < task.waiting_until:
+                # We're ahead of schedule; wait until it's time to resume.
+                delta = task.waiting_until - now
+                time.sleep(delta.total_seconds())
+                now = datetime.datetime.now()
+            try:
+                # It's time to resume the coroutine.
+                wait_until = task.coro.send(now)
+                heapq.heappush(self._waiting, Task(wait_until, task.coro))
+            except StopIteration:
+                # The coroutine is done.
+                pass
+
+
+@types.coroutine
+def sleep(seconds):
+    now = datetime.datetime.now()
+    wait_until = now + datetime.timedelta(seconds=seconds)
+    actual = yield wait_until
+    return actual - now
+
+
+async def countdown(label, length, *, delay=0):
+    print(label, 'waiting', delay, 'seconds before starting countdown')
+    delta = await sleep(delay)
+    print(label, 'starting after waiting', delta)
+    while length:
+        print(label, 'T-minus', length)
+        waited = await sleep(1)
+        length -= 1
+    print(label, 'lift-off!')
+
+
+def main():
+    loop = SleepingLoop(countdown('A', 5), countdown('B', 3, delay=2),
+                        countdown('C', 4, delay=1))
+    start = datetime.datetime.now()
+    loop.run_until_complete()
+    print('Total elapsed time is', datetime.datetime.now() - start)
+
+if __name__ == '__main__':
+    main()
