5f5181734d4b764db42fbb02e43173d015cb9083
guozeliang <932356614@qq.com>
2018-06-08 18:58:11 +0800

    添加 日志输出文件。

--- login_test/new_test.py
+++ login_test/new_test.py
@@ -2,22 +2,24 @@ from gevent import monkey; monkey.patch_all()
 import gevent,requests
 import time
 from aes_tool import aes_tool
-count = 0
+from logger import Logger
+
+myLogger = Logger(logger="request").getlog()
 
 def f(url):
     data = requests.get(url)
     if data.status_code == 200:
-        print("one")
+        myLogger.info("success")
 
 
 start = time.time()
 url_list = []
 endUrl = '/app/phpatientarticle/appuseagreement?aArticleCategoryId=47DE670DABEA41838356283C6E212435'
 
-for i in range(1):
+for i in range(1000):
     enStr = 'http://app.gsfybjy.com/phpatient/app/appservice?querystr=' + aes_tool.encrypy(endUrl).decode('utf-8')
     url_list.append(gevent.spawn(f,enStr))
-print(enStr)
+myLogger.info(enStr)
 gevent.joinall(url_list)
-print (time.time() - start)
-print('---------end----------')
+myLogger.info(time.time() - start)
+myLogger.info('---------end----------')
